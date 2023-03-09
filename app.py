dc21885e6dcbe3b684cbc3ea155d8fbb511b6a1f
Harshada-khuspe <126970098+Harshada-khuspe@users.noreply.github.com>
2023-03-09 17:40:17 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+st.title(":red[Data scince] internship-Data app")
+st.header("Hi, I'm :red[Harshada Narayan Khuspe ]") 
+st.subheader("A Developer")
+st.write("click here for [linkedin](https://www.linkedin.com/in/harshada-khuspe-06394a225)")
+st.write("click here for [github](https://github.com/Harshada-khuspe)")
+st.snow()
+
+btn_click = st.button("Click Me!")
+
+if btn_click == True:
+    st.subheader("You clicked me :cry:")
+    st.balloons()
