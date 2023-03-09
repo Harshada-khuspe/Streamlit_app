44824b774ab239837c4f307f45f206ed73b691d0
Harshada-khuspe <126970098+Harshada-khuspe@users.noreply.github.com>
2023-03-09 17:56:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.title(":red[Data scince] internship-Data app")
 st.header("Hi, I'm :red[Harshada Narayan Khuspe ]") 
 st.subheader("A Developer")
-st.subheader("From Satara")
+st.subheader("From :red[Satara]")
 st.write("click here for [linkedin](https://www.linkedin.com/in/harshada-khuspe-06394a225)")
 st.write("click here for [github](https://github.com/Harshada-khuspe)")
 st.snow()
